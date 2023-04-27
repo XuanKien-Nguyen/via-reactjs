d5de1865e8be7f3a5d8208c09f43f7580087a407
huytran <huyth2403@gmail.com>
2023-04-27 15:59:10 +0700

    product list

--- src/components/pages/common/ListLayout.js
+++ src/components/pages/common/ListLayout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Icon} from 'antd';
+import {useHistory} from "react-router-dom";
 
-const ListLayout = ({children, titleCategory, contentSeeMore}) => {
+const ListLayout = ({categoryId, children, titleCategory, contentSeeMore}) => {
 
-  return (
+    const history = useHistory()
+
+    return (
     <div className='category-container'>
-        <div className='category-title'><h3 className='title-content'><b><span>{titleCategory}</span></b><a>{contentSeeMore}<Icon type="right" /></a></h3></div>
+        <div className='category-title'><h3 className='title-content'><b><span>{titleCategory}</span></b><a onClick={() => history.push(`/category?id=${categoryId}`)}>{contentSeeMore}<Icon type="right" /></a></h3></div>
         <div className='category-grid'>
           {children}
         </div>
--- src/components/pages/homepage/components/products/ProductLayout.js
+++ src/components/pages/homepage/components/products/ProductLayout.js
@@ -3,9 +3,9 @@ import ListLayout from '../../../common/ListLayout';
 import ProductCard from './ProductCard';
 
 const ProductLayout = ({categoryParent}) => {
-  return (
+    return (
     <section id='product-layout'>
-      <ListLayout titleCategory={categoryParent.name} contentSeeMore={'Xem thêm !'} cardComponent={<ProductCard />}>
+      <ListLayout titleCategory={categoryParent.name} contentSeeMore={'Xem thêm !'} categoryId={categoryParent.id} cardComponent={<ProductCard />}>
         {categoryParent.childCategoryList.map(product => <ProductCard key={product.id} productDetail={product}/>)}
       </ListLayout>
     </section>
