91a9913c992d519e9733f2b6e33f86c21f85b2c7
kienpc9x <kienpc9x@gmail.com>
2023-04-30 17:16:12 +0700

    fix slider

--- src/components/pages/homepage/components/slider/SlideLayout.js
+++ src/components/pages/homepage/components/slider/SlideLayout.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Carousel, Button, Icon } from 'antd';
 
 const SlideLayout = () => {
-  const onChange = (currentSlide) => {
-  };
   return (
     <section id="slider-layout">
-      <Carousel afterChange={onChange} style={{ marginBottom: '30px' }} dots arrows autoplay>
+      <Carousel dots arrows>
         <div className='slide-item'>
-          <div className='slide-text'>
+          <div className='slide-item-container'>
+            <div className='slide-text'>
               <div>
                 <h1 className='slide-title'>Nền tảng mua bán Via Facebook, Clone, BM, Fanpage và tài nguyên số 1 Việt Nam</h1>
                 <span className='slide-content'>Giao dịch hoàn toàn tự động, nhân viên hỗ trợ nhanh chóng. Tài nguyên an toàn, bảo mật.</span>
@@ -17,16 +16,28 @@ const SlideLayout = () => {
                 console.log('click button')
               }}>
                 XEM CHI TIẾT
-                <Icon type="right" style={{fontSize: '12px'}}/>
+                <Icon type="right" style={{ fontSize: '12px' }} />
               </Button>
+            </div>
+            <img alt='slide-item' src={require('../../../../../assets/img/slider-test.png')} />
           </div>
-          <img alt='slide-item' src={require('../../../../../assets/img/slider-test.png')}/>
         </div>
-        <div>
-          <h1>2</h1>
-        </div>
-        <div>
-          <h1>3</h1>
+        <div className='slide-item'>
+          <div className='slide-item-container'>
+            <div className='slide-text'>
+              <div>
+                <h1 className='slide-title'>SLIDE 2</h1>
+                <span className='slide-content'>Content</span>
+              </div>
+              <Button type="primary" size='large' onClick={(e) => {
+                console.log('click button')
+              }}>
+                XEM CHI TIẾT
+                <Icon type="right" style={{ fontSize: '12px' }} />
+              </Button>
+            </div>
+            <img alt='slide-item' src={require('../../../../../assets/img/slider-test.png')} />
+          </div>
         </div>
       </Carousel>
     </section>
