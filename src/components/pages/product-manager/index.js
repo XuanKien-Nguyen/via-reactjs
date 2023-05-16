c1c35626c37d6c94967b1ca59c719daaeff0046b
hoangth <hoangth2403@gmail.com>
2023-05-16 17:34:55 +0700

    create new product

--- src/components/pages/product-manager/index.js
+++ src/components/pages/product-manager/index.js
@@ -6,31 +6,54 @@ import {message} from "antd";
 import TableCommon from "../../common/table";
 import {convertCurrencyVN} from "../../../utils/helpers";
 import FilterItem from "../category/components/filter/FilterItem";
-
+// import Form from "../category-manager/components/create-category/form";
+import Modal from "antd/es/modal";
+import {Form, Input, Select} from "antd";
+import TextArea from "antd/es/input/TextArea";
+import {getCategoryList} from "../../../services/category/category";
+const { Option } = Select;
 const {Panel} = Collapse
 const dateFormat = 'YYYY-MM-DD';
-let debounce = null
-
 function Index() {
     const {t} = useTranslation()
     const [productList, setProductList] = useState([])
     const [categoryId, setCategoryId] = useState('')
-    const [uuid, setUUID] = useState('');
+    const [uid, setUid] = useState('');
     const [createdTime, setCreatedTime] = useState([])
     const [page, setPage] = useState({
         perpage: 10,
         currentPage: 1,
         total: 0
     })
+    const [categoryList, setCategoryList] = useState([]);
+    const [product, setProduct] = useState({
+        category_id: '',
+        product_text: '',
+        cost: 0
+    })
+
+    const [visible, setVisible] = useState(false)
     const [createdBy, setCreatedBy] = useState('')
 
     useEffect(() => {
-        clearTimeout(debounce);
-        debounce = setTimeout(() => {
-            console.log(createdTime);
+        getCategoryList().then((resp) => {
+            console.log('category: ', resp);
+            if(resp.status === 200){
+                setCategoryList(resp?.data?.categoryListFound || []);
+            }
+        }).catch(() => {
+
+        }).finally(() => {
+
+        })
+    }, [])
+
+    useEffect(() => {
+        let created_time = createdTime.length > 0 ? JSON.stringify(createdTime?.map(el => el?.format(dateFormat))) : "";
             const body = {
                 category_id: categoryId,
-                uuid: uuid,
+                uid,
+                created_time,
                 createdby: createdBy,
                 page: page.currentPage,
                 perpage: page.perpage
@@ -43,7 +66,6 @@ function Index() {
 
             getProductList(body).then((resp) => {
                 console.log(resp);
-                console.log(resp.status);
                 if (resp.status === 200) {
                     const pageInfo = {
                         total: resp?.data?.totalProducts,
@@ -56,13 +78,35 @@ function Index() {
                 }
             }).catch(() => message.error('Có lỗi xảy ra khi lấy sản phẩm')).finally(() => {
             });
-        }, 500)
-
-    }, [categoryId, uuid, createdTime, createdBy])
+    }, [categoryId, uid, createdTime, createdBy])
 
     const onReset = () => {
 
     }
+
+    const getCategoryListOptions = () =>{
+        return categoryList.map(el => ({
+            label: el.name,
+            value: el.id
+        }))
+    }
+
+    const createNewProduct = () =>{
+        setVisible(true)
+        return false
+    }
+
+    const onChangeCategory = (value) =>{
+        console.log(value);
+    }
+
+    const handleSubmit = (resp) =>{
+        console.log('submit', resp)
+    }
+
+    const onFinish = (values) => {
+        console.log(values);
+    };
 
     const columns = [
         {
@@ -75,14 +119,12 @@ function Index() {
         {
             title: 'Tên danh mục',
             dataIndex: 'category_name',
-            // render: v => `#${v}`,
             width: '150px',
             align: 'center',
         },
         {
-            title: 'UUID',
+            title: 'UID',
             dataIndex: 'uid',
-            // render: v => `#${v}`,
             width: '150px',
             align: 'center',
         },
@@ -126,22 +168,20 @@ function Index() {
                     <Panel key={1} className='filter-container' header={<div className='filter-header'>
                         <div><Icon type="filter" theme="filled"/>&nbsp;{t('filter.title')}</div>
                     </div>}>
-                        {/*{items}*/}
-
-                        <FilterItem defaultValue={categoryId} setValue={setCategoryId} type={'text'}
+                        <FilterItem defaultValue={categoryId} setValue={setCategoryId} options={getCategoryListOptions()} type={'select'}
                                     title='ID danh mục'/>
                         <FilterItem defaultValue={createdTime} setValue={setCreatedTime} type={'date'}
                                     placeholder={[t('filter.from'), t('filter.to')]} title={t('filter.date')}/>
 
-                        <FilterItem defaultValue={uuid} setValue={setUUID} type={'text'} title='UUID'/>
+                        <FilterItem defaultValue={uid} setValue={setUid} type={'text'} title='UUID'/>
                         <FilterItem defaultValue={createdBy} setValue={setCreatedBy} type={'text'} title={t('filter.created-by')}/>
-
                     </Panel>
                 </Collapse>
                 <Button className='reset-filter-btn' type="primary" size='small' icon="reload"
                         onClick={onReset}>{t('common.reset')}</Button>
             </div>
-
+            <Button className='reset-filter-btn' type="primary" size='small' icon="reload"
+                    onClick={createNewProduct}>{'Thêm mới sản phẩm'}</Button>
             <TableCommon className='table-order'
                          bordered={true}
                          rowKey="id"
@@ -149,6 +189,53 @@ function Index() {
                          setPage={setPage}
                          datasource={productList}
                          columns={columns}/>
+
+
+            <Modal
+                centered
+                width={'50%'}
+                closable={false}
+                visible={visible}
+                maskClosable={false}
+                title={'Thêm mới sản phẩm'}
+                onCancel={() => () => {
+                    setVisible(false)
+                }}
+                footer={[
+                    <Button key="back" disabled={false} onClick={() => {
+                        setVisible(false)
+                    }}>
+                        Huỷ bỏ
+                    </Button>,
+                    <Button htmlType="submit" type="primary" loading={false}>
+                        Thêm mới
+                    </Button>
+                ]}>
+
+                <Form>
+                    <Form.Item name="category_id" rules={[{ required: true , message: 'Vui lòng chọn mã danh mục'}]}>
+                        <Select defaultValue={product.category_id}>
+                            {getCategoryListOptions().map((el, idx) => <Option key={idx} value={el.value}>{el.label}</Option>)}
+                        </Select>
+                    </Form.Item>
+                    <Form.Item name="product_text" rules={[{ required: true, message: 'Vui lòng nhâp mã sản phẩm' }]}>
+                        <TextArea defaultValue={product.product_text} placeholder="Mã sản phẩm"/>
+                    </Form.Item>
+                    <Form.Item name="cost" rules={[{ required: true, message: 'Vui lòng nhập giá' }]}>
+                        <Input defaultValue={product.cost} type={'number'} placeholder="Giá"/>
+                    </Form.Item>
+                    <Button onClick={handleSubmit}>
+                        Submit
+                    </Button>
+                </Form>
+
+                {/*<Form setVisible={setVisible}*/}
+                {/*      reload={reload}*/}
+                {/*      setPending={setPending}*/}
+                {/*      visible={visible}*/}
+                {/*/>*/}
+            </Modal>
+
 
         </div>
 
