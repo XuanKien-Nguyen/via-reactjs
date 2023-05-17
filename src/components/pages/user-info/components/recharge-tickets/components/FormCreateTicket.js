a46612deb5f9ab853b93d2a04127d74023934192
kienpc9x <kienpc9x@gmail.com>
2023-05-17 15:12:42 +0700

    add beforeUpload

--- src/components/pages/user-info/components/recharge-tickets/components/FormCreateTicket.js
+++ src/components/pages/user-info/components/recharge-tickets/components/FormCreateTicket.js
@@ -60,6 +60,33 @@ const Wrapper = (props) => {
         setPreviewVisible(true)
     }
 
+    const beforeUpload = (file) => {
+        const extension = file.name.split('.')
+
+        const errorFn = () => {
+            const els = document.querySelector('.ant-upload-list-item-card-actions a i')
+            if (els) {
+                els.click()
+            }
+        }
+        if (extension.length > 1) {
+            const isJpgOrPng = extension[extension.length - 1] === 'jpg' || extension[extension.length - 1] === 'png';
+            if (!isJpgOrPng) {
+                Modal.error({
+                    content: t('recharge-tickets.modal-image-hint'),
+                    onOk: errorFn
+                });
+            }
+            if (file.size / 1024 / 1024 > 1) {
+                Modal.error({
+                    content: t('recharge-tickets.modal-image-size'),
+                    onOk: errorFn
+                });
+            }
+        }
+        return false
+    }
+
     return <Fragment>
         {props.visible && <Form onSubmit={handleSubmit}>
                 
@@ -79,20 +106,22 @@ const Wrapper = (props) => {
                         <Upload.Dragger
                             name="files"
                             multiple={false}
-                            beforeUpload={() => false}
+                            beforeUpload={beforeUpload}
                             accept={'.png, .jpg'}
                             onPreview={handlePreview}
                             listType="picture"
+                            id={'upload-file_recharge-tickets'}
                         >
                             <p className="ant-upload-drag-icon">
                                 <Icon type="inbox"/>
                             </p>
                             <p className="ant-upload-text">{t('recharge-tickets.modal-image-text')}</p>
                             <p className="ant-upload-hint">{t('recharge-tickets.modal-image-hint')}</p>
+                            <p className="ant-upload-hint">{t('recharge-tickets.modal-image-only')} - {t('recharge-tickets.modal-image-size')}</p>
                         </Upload.Dragger>,
                     )}
                     <Modal visible={previewVisible} footer={null} onCancel={() => setPreviewVisible(false)}>
-                        <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                        <img style={{ width: '100%' }} src={previewImage} />
                     </Modal>
                 </Form.Item>
             <Button id={'submit-create-ticket'} type="primary" htmlType="submit" hidden></Button>
