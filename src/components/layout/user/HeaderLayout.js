797249fd594ba4f02be2e757f05d02cf843632eb
huytran <huyth2403@gmail.com>
2023-05-31 23:05:19 +0700

    fix bug

--- src/components/layout/user/HeaderLayout.js
+++ src/components/layout/user/HeaderLayout.js
@@ -23,7 +23,7 @@ function HeaderLayout({ history }) {
 
     const categories = useSelector(store => store.categories)
 
-    const [userInfo, setUserInfo] = useState()
+    // const [userInfo, setUserInfo] = useState()
     const [visible, setVisible] = useState(false)
 
     const { setLoading } = useContext(LayoutContext);
@@ -49,7 +49,7 @@ function HeaderLayout({ history }) {
         await logout();
         dispatch({ type: "LOGOUT" })
         localStorage.removeItem("is_logged")
-        localStorage.removeItem('user_info')
+        // localStorage.removeItem('user_info')
         window.location.href = '/'
         setLoading(false)
     }
@@ -64,10 +64,10 @@ function HeaderLayout({ history }) {
             });
         }
 
-        const u = localStorage.getItem('user_info')
-        if (u) {
-            setUserInfo(JSON.parse(u))
-        }
+        // const u = localStorage.getItem('user_info')
+        // if (u) {
+        //     setUserInfo(JSON.parse(u))
+        // }
     }, []);
 
     const menu = (
@@ -92,7 +92,7 @@ function HeaderLayout({ history }) {
         )
         return <Dropdown overlay={menu}>
             <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-                {userInfo.username?.toUpperCase()} <Icon type="down" />
+                {user?.username?.toUpperCase()} <Icon type="down" />
             </a>
         </Dropdown>
     }
@@ -117,7 +117,7 @@ function HeaderLayout({ history }) {
     }
 
     const goToRecharge = () => {
-        if (!userInfo) {
+        if (!user) {
             history.push('/login')
             return
         } 
@@ -171,11 +171,11 @@ function HeaderLayout({ history }) {
                             {user && <li className='item header_user-balance'><b>{t('profile.balance')}:&nbsp;{convertCurrencyVN(user?.amount_available)}</b></li>}
                             {/* {user && <li className='item header_user-bonus'><b>{t('profile.bonus')}:&nbsp;{convertCurrencyVN(user?.bonus)}</b></li>} */}
                             <li className='header-devider'></li>
-                            <li className='item' style={userInfo?.role !== 'admin' ? { display: 'none' } : {}}><div className='notify'><Icon type="bell" theme="filled" style={{ fontSize: '20px', width: '20px', height: '20px' }} /></div></li>
+                            <li className='item' style={user?.role !== 'admin' ? { display: 'none' } : {}}><div className='notify'><Icon type="bell" theme="filled" style={{ fontSize: '20px', width: '20px', height: '20px' }} /></div></li>
                             <li style={{
                                 height: '64px',
                                 display: 'flex',
-                            }} className='item'><div className='signin-signup d-flex justify-content-center align-items-center' style={{ height: '100%' }}>{userInfo ? dropDownUser() : <Fragment><div className={'login-home uppercase'} onClick={() => goto('/login')}>{t('common.sign-in')}</div><div className={'register-home uppercase'} onClick={() => goto('/register')}>{t('common.sign-up')}</div></Fragment>}</div></li>
+                            }} className='item'><div className='signin-signup d-flex justify-content-center align-items-center' style={{ height: '100%' }}>{user ? dropDownUser() : <Fragment><div className={'login-home uppercase'} onClick={() => goto('/login')}>{t('common.sign-in')}</div><div className={'register-home uppercase'} onClick={() => goto('/register')}>{t('common.sign-up')}</div></Fragment>}</div></li>
                         </ul>
                     </div>
                 </div>
